e957cd9eb6d1949d676018d6690ae1a5114ad4e0
Treek <nbatre1@yahoo.com>
2020-03-23 19:01:21 -0500

    keep moving forward

--- map1.py
+++ map1.py
@@ -7,6 +7,18 @@ data = pandas.read_csv("Volcanoes.txt")
 
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+
+def color_producer(el):
+   if el < 1000:
+       return 'green'
+   elif  1000 <= el < 3000:
+       return 'orange'
+   else:
+       return 'red'
+
+
 
 map = folium.Map(location = [38.58, -99.09], zoom_start = 6, tiles = "Stamen Terrain")
 
@@ -17,15 +29,15 @@ fg = folium.FeatureGroup(name="My Map")
 # fg.add_child((folium.Marker(location = [38.2, 99.1], popup = "Hi I am here", icon=folium.Icon(color='green'))))
 
 
-for lt,ln in zip(lat,lon): 
 
-  fg.add_child((folium.Marker(location = [lt,ln], popup = "Hi I am here", icon=folium.Icon(color='green'))))
+for lt,ln,el in zip(lat,lon,elev): 
+
+  fg.add_child((folium.Marker(location = [lt,ln], popup = el, icon=folium.Icon(color=color_producer(el)))))
 
 
 
 # fg.add_child((folium.Marker(location = [35.2, 99.1], popup = "Hi I am here", icon=folium.Icon(color='blue'))))
 # map.add_child(folium.Marker(location = [38.2, 99.1], popup = "Hi I am here", icon=folium.Icon(color='green')))
-
 
 
 map.add_child(fg)
